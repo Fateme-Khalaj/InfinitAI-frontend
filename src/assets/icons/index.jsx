7c11942c9975845fa0b50d1948708a7bf7dc1d86
Fateme Khalaj <fateme.m.a.khalaj@gmail.com>
2025-01-04 22:48:17 +0330

    live chat widget added

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -1,11 +1,13 @@
 import Logo from "./Logo.png";
 import arrowDown from "./arrowDown.svg";
 import arrowRight from "./arrowRight.svg";
+import liveChatIcon from "./live-chat.svg";
 
 const icons = {
   Logo,
   arrowDown,
   arrowRight,
+  liveChatIcon,
 };
 
 export default icons;
